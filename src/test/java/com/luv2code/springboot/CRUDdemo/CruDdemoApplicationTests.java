94fb3eb7183a1ecfebda56c45f6fa09b2cb6083d
unknown <pinkeshthakker@gmail.com>
2019-10-29 20:50:10 -0400

    Second commit

---
+++ src/test/java/com/luv2code/springboot/CRUDdemo/CruDdemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.CRUDdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CruDdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Test case::");
+	}
+	
+
+}
